7e2c5cef8a8757f19d41fe1fc11aeac29084e356
woojiahao <woojiahao1234@gmail.com>
2019-03-06 12:23:56 +0800

    Added exceptions if the environment variables are not configured

--- src/main/kotlin/configuration/Configuration.kt
+++ src/main/kotlin/configuration/Configuration.kt
@@ -1,4 +1,6 @@
 package configuration
 
 val botToken = System.getenv("BOT_TOKEN")
+    ?: throw IllegalStateException("Bot token must be supplied as BOT_TOKEN environment variable")
 val meetupApiToken = System.getenv("MEETUP_API_KEY")
+    ?: throw IllegalStateException("Meetup api key must be supplied as MEETUP_API_KEY environment variable")

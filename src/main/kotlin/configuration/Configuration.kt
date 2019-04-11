37f00ef64593800bca1405202e87183403a5b1e9
woojiahao <woojiahao1234@gmail.com>
2019-04-11 08:13:52 +0800

    Added database url env var for heroku

--- src/main/kotlin/configuration/Configuration.kt
+++ src/main/kotlin/configuration/Configuration.kt
@@ -4,8 +4,5 @@ val botToken = System.getenv("BOT_TOKEN")
     ?: throw IllegalStateException("Bot token must be supplied as BOT_TOKEN environment variable")
 val meetupApiToken = System.getenv("MEETUP_API_KEY")
     ?: throw IllegalStateException("Meetup api key must be supplied as MEETUP_API_KEY environment variable")
-
-val channels = System
-  .getenv()
-  .filterKeys { it.startsWith("BOT_CHANNEL_") }
-  .mapNotNull { it.value }
+val databaseUrl = System.getenv("JDBC_DATABASE_URL")
+    ?: throw java.lang.IllegalStateException("Heroku postgres add on must be configured")

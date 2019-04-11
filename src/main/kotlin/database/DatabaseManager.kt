f52aceeb2b158df8b21154d7602c65deaa7b4f0b
woojiahao <woojiahao1234@gmail.com>
2019-04-11 08:03:29 +0800

    Added db management
    Added db management for registering channels to post updates to

---
+++ src/main/kotlin/database/DatabaseManager.kt
@@ -0,0 +1,20 @@
+package database
+
+import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.SchemaUtils
+import org.jetbrains.exposed.sql.Table
+import org.jetbrains.exposed.sql.transactions.transaction
+
+fun setup(databaseUrl: String) {
+  Database.connect(databaseUrl, "org.postgresql.Driver")
+
+  transaction {
+    SchemaUtils.create(RegisteredChannels)
+  }
+}
+
+object RegisteredChannels : Table() {
+  val channelId = varchar("channel_id", 20).primaryKey()
+  val serverId = varchar("server_id", 20)
+  val isEnabled = bool("is_enabled").default(true)
+}
---
+++ src/main/kotlin/database/RegisteredChannelOperations.kt
@@ -0,0 +1,30 @@
+package database
+
+import org.jetbrains.exposed.sql.deleteWhere
+import org.jetbrains.exposed.sql.insert
+import org.jetbrains.exposed.sql.transactions.transaction
+import org.jetbrains.exposed.sql.update
+
+fun registerChannel(channelId: String, serverId: String) =
+  transaction {
+    RegisteredChannels.insert {
+      it[RegisteredChannels.channelId] = channelId
+      it[RegisteredChannels.serverId] = serverId
+    }
+  }
+
+fun unregisterChannel(channelId: String) =
+  transaction {
+    RegisteredChannels.deleteWhere { RegisteredChannels.channelId eq channelId }
+  }
+
+fun disableChannel(channelId: String) = toggleChannel(channelId, false)
+
+fun enableChannel(channelId: String) = toggleChannel(channelId, true)
+
+private fun toggleChannel(channelId: String, state: Boolean) =
+  transaction {
+    RegisteredChannels.update({ RegisteredChannels.channelId eq channelId }) {
+      it[RegisteredChannels.isEnabled] = state
+    }
+  }

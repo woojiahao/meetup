9ab6b39bc4682b541fa2c7aad6b5a7ef4ff66a46
woojiahao <woojiahao1234@gmail.com>
2019-04-11 09:17:58 +0800

    Updated daily posting code

--- src/main/kotlin/Bot.kt
+++ src/main/kotlin/Bot.kt
@@ -1,36 +1,53 @@
+import commands.eventsEmbed
 import configuration.botToken
 import configuration.databaseUrl
+import database.getRegisteredChannels
 import database.setup
 import me.aberrantfox.kjdautils.api.startBot
 import meetup.Meetup
 import net.dv8tion.jda.core.JDA
+import utility.singaporeDateTime
+import utility.singaporeZone
+import java.awt.Color
+import java.util.*
+import kotlin.concurrent.timerTask
 
 private val meetup = Meetup()
 
 fun main() {
   setup(databaseUrl)
-  val jda = startBot(botToken) { configure { prefix = "!" } }.jda
-//  configureDailyPost(jda)
+  println("Database setup")
+  val jda = startBot(botToken) {
+    configure {
+      prefix = "!"
+    }
+  }.jda
+  println("Bot setup")
+  println("Starting daily post task")
+  configureDailyPost(jda)
 }
 
 private fun configureDailyPost(jda: JDA) {
-//  with(Calendar.getInstance(TimeZone.getTimeZone(singaporeZone))) {
-//    set(Calendar.HOUR_OF_DAY, 10)
-//    set(Calendar.MINUTE, 0)
-//    set(Calendar.SECOND, 0)
-//
-//    val postTask = timerTask {
-//      val todayEvents = meetup.findUpcomingEvents(20, listOf(292), singaporeDateTime)
-//      channels.forEach { channel ->
-//        jda.getTextChannelById(channel).sendMessage(
-//          eventsEmbed(
-//            "Events happening today",
-//            Color.decode("#42A5F5"),
-//            todayEvents
-//          )
-//        ).queue()
-//      }
-//    }
-//    Timer().schedule(postTask, time, TimeUnit.MILLISECONDS.convert(1, TimeUnit.DAYS))
-//  }
+  with(Calendar.getInstance(TimeZone.getTimeZone(singaporeZone))) {
+    set(Calendar.HOUR_OF_DAY, 10)
+    set(Calendar.MINUTE, 0)
+    set(Calendar.SECOND, 0)
+
+    println("Daily posts at ${this[Calendar.HOUR]}")
+
+    val postTask = timerTask {
+      println("Daily post")
+      val todayEvents = meetup.findUpcomingEvents(20, listOf(292), singaporeDateTime)
+      getRegisteredChannels().forEach {
+        jda.getTextChannelById(it.channelId).sendMessage(
+          eventsEmbed(
+            "Events happening today",
+            Color.decode("#42A5F5"),
+            todayEvents
+          )
+        ).queue()
+      }
+    }
+    Timer().schedule(postTask, time)
+  }
 }

b0255056bd2b7c9074198a9b2faecfcd21314113
woojiahao <woojiahao@gmail.com>
2019-09-04 18:41:35 +0800

    Added some formtting for prompts

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-read -p "Enter your application name" applicationName
+read -p "Enter your application name: " applicationName
 heroku create "$applicationName"
 heroku addons:create heroku-postgresql:hobby-dev
-read -p "Enter your bot token" botToken
+read -p "Enter your bot token: " botToken
 heroku config:set BOT_TOKEN="$botToken"
 heroku stack:set container
-heroku push heroku master
+git push heroku master

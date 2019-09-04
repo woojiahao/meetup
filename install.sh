9adc852724b90de9de4f49a6388012c39ad81aa6
woojiahao <woojiahao@gmail.com>
2019-09-04 20:21:06 +0800

    Added prompts for db user/pass

--- install.sh
+++ install.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 read -p "Enter your application name: " applicationName
 heroku create "$applicationName"
+
 heroku addons:create heroku-postgresql:hobby-dev
-read -p "Enter your bot token: " botToken
+
+read -p "Enter your database username provisioned by Heroku: " databaseUsername
+heroku config:set DATABASE_USERNAME="$databaseUsername"
+
+read -sp "Enter your database password provisioned by Heroku: " databasePassword
+heroku config:set DATABASE_PASSWORD="$databasePassword"
+
+read -sp "Enter your bot token: " botToken
 heroku config:set BOT_TOKEN="$botToken"
+
 heroku stack:set container
+
 git push heroku master
+
 heroku ps:scale worker=1

f7503818a3cee7ad221cd376e34be3a41f6bcc62
woojiahao <woojiahao@gmail.com>
2019-09-04 21:36:51 +0800

    Removed whitespace

--- install.sh
+++ install.sh
@@ -10,13 +10,8 @@ prompt_env_then_set() {
 
 read -p "Enter your application name: " applicationName
 heroku create "$applicationName"
-
 heroku addons:create heroku-postgresql:hobby-dev
-
 prompt_env_then_set "Enter your bot token: " "BOT_TOKEN"
-
 heroku stack:set container
-
 git push heroku master
-
 heroku ps:scale worker=1

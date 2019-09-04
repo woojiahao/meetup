cac85c7f40c0b548a9fbd26cb1746f4df691e923
Woo Jia Hao <woojiahao@gmail.com>
2019-09-04 09:43:54 +0800

    Added DateUtility for extracting timezone string

--- src/main/kotlin/utility/DateUtility.kt
+++ src/main/kotlin/utility/DateUtility.kt
@@ -1,10 +1,17 @@
 package utility
 
+import java.text.SimpleDateFormat
 import java.time.ZoneId
 import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
+import java.util.*
 
 val singaporeZone = ZoneId.of("Asia/Singapore") ?: throw IllegalArgumentException("Zone ID for Singapore invalid")
 
 val singaporeDateTime: ZonedDateTime
   get() = ZonedDateTime.now(singaporeZone)
 
+fun extractTimezoneDate(timezoneString: String): Date =
+  SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssX")
+    .apply { timeZone = TimeZone.getTimeZone("GMT") }
+    .parse(timezoneString)

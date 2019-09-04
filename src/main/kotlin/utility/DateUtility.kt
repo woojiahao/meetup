230dec6e82a68edb43cdc91075f75ba8a4b4eb22
woojiahao <woojiahao@gmail.com>
2019-09-04 16:26:31 +0800

    Added extension properties for getting date/time

--- src/main/kotlin/utility/DateUtility.kt
+++ src/main/kotlin/utility/DateUtility.kt
@@ -1,11 +1,9 @@
 package utility
 
-import java.text.SimpleDateFormat
 import java.time.ZoneId
 import java.time.ZoneOffset
 import java.time.ZonedDateTime
 import java.time.format.DateTimeFormatter
-import java.util.*
 
 val singaporeZone = ZoneId.of("Asia/Singapore") ?: throw IllegalArgumentException("Zone ID for Singapore invalid")
 
@@ -13,8 +11,14 @@ val singaporeDateTime: ZonedDateTime
   get() = ZonedDateTime.now(singaporeZone)
 
 fun extractTimezoneDate(timezoneString: String): ZonedDateTime =
-  ZonedDateTime.parse(timezoneString, DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ssXXXXX"))
+    ZonedDateTime.parse(timezoneString, DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ssXXXXX"))
 
 fun ZonedDateTime.toSingaporeDateTime(): ZonedDateTime =
     if (offset == ZoneOffset.ofHours(8)) this
     else withZoneSameInstant(ZoneId.ofOffset("", ZoneOffset.ofHours(8)))
+
+val ZonedDateTime.date
+  get() = format(DateTimeFormatter.ofPattern("dd-MM-yyyy"))
+
+val ZonedDateTime.time
+  get() = format(DateTimeFormatter.ofPattern("hh.mma"))

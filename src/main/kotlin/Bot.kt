6200066f2a2d633da749438b28a24ab1680e4e46
woojiahao <woojiahao@gmail.com>
2019-09-04 20:30:49 +0800

    JDBC url constructed manually

--- src/main/kotlin/Bot.kt
+++ src/main/kotlin/Bot.kt
@@ -1,9 +1,14 @@
 import configuration.botToken
+import configuration.databasePassword
 import configuration.databaseUrl
+import configuration.databaseUsername
 import database.setup
 import me.aberrantfox.kjdautils.api.startBot
 
 fun main() {
-  setup(databaseUrl)
+  setup(jdbcDatabaseUrl)
   startBot(botToken) { configure { prefix = "!" } }
 }
+
+private val jdbcDatabaseUrl
+  get() = "jdbc:$databaseUrl?user=$databaseUsername&password=$databasePassword"

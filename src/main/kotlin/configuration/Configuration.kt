e55a240da1400862f9261ac3322eb97abab84c06
woojiahao <woojiahao@gmail.com>
2019-09-04 19:10:04 +0800

    Removed JDBC url access as no buildpack

--- src/main/kotlin/configuration/Configuration.kt
+++ src/main/kotlin/configuration/Configuration.kt
@@ -2,5 +2,5 @@ package configuration
 
 val botToken = System.getenv("BOT_TOKEN")
     ?: throw IllegalStateException("Bot token must be supplied as BOT_TOKEN environment variable")
-val databaseUrl = System.getenv("JDBC_DATABASE_URL")
+val databaseUrl = System.getenv("DATABASE_URL")
     ?: throw java.lang.IllegalStateException("Heroku postgres add on must be configured")

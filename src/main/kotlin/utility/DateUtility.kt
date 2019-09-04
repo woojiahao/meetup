6be4ab682c6865506b49542baec2c11c82a90e9d
woojiahao <woojiahao@gmail.com>
2019-09-04 16:10:21 +0800

    Added utility to convert time to Singapore offset  Please enter the commit message for your changes. Lines starting

--- src/main/kotlin/utility/DateUtility.kt
+++ src/main/kotlin/utility/DateUtility.kt
@@ -2,6 +2,7 @@ package utility
 
 import java.text.SimpleDateFormat
 import java.time.ZoneId
+import java.time.ZoneOffset
 import java.time.ZonedDateTime
 import java.time.format.DateTimeFormatter
 import java.util.*
@@ -14,3 +15,6 @@ val singaporeDateTime: ZonedDateTime
 fun extractTimezoneDate(timezoneString: String): ZonedDateTime =
   ZonedDateTime.parse(timezoneString, DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ssXXXXX"))
 
+fun ZonedDateTime.toSingaporeDateTime(): ZonedDateTime =
+    if (offset == ZoneOffset.ofHours(8)) this
+    else withZoneSameInstant(ZoneId.ofOffset("", ZoneOffset.ofHours(8)))
--- src/test/kotlin/utility/DateUtilityTest.kt
+++ src/test/kotlin/utility/DateUtilityTest.kt
@@ -1,8 +1,6 @@
 package utility
 
-import java.time.ZoneId
 import java.time.ZoneOffset
-import java.util.*
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
@@ -17,5 +15,12 @@ class DateUtilityTest {
     assertEquals(30, dateTime.minute)
     assertEquals(0, dateTime.second)
     assertEquals(ZoneOffset.ofHours(8), dateTime.offset)
+  }
+
+  @Test
+  fun `ZonedDateTime is converted to Singapore date time`() {
+    val japanTime = extractTimezoneDate("2019-09-03T18:30:00+09:00")
+    val singaporeTime = extractTimezoneDate("2019-09-03T17:30:00+08:00")
+    assertEquals(singaporeTime, japanTime.toSingaporeDateTime())
   }
 }

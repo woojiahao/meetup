8c757456ebdfd9e0e23df66111a9c67df6c7054a
woojiahao <woojiahao@gmail.com>
2019-09-05 11:08:58 +0800

    Added echos to local install script

--- local-install.sh
+++ local-install.sh
@@ -3,10 +3,13 @@ docker build -t sg-meetup-discord .
 docker image ls
 
 if [[ -z "$BOT_TOKEN" ]]; then
-  read -p "Enter your bot token" BOT_TOKEN
+  read -p "Enter your bot token: " token
+  echo "Saving bot token :: $token"
+  export BOT_TOKEN="$token"
+  echo "Bot token stored as :: $BOT_TOKEN"
 else
   echo "Bot token to use :: $BOT_TOKEN"
 fi
 
-docker run -e BOT_TOKEN="$BOT_TOKEN" -e DATABASE_URL="$(heroku config:get DATABASE_URL)"
+docker run -e BOT_TOKEN="$BOT_TOKEN" -e DATABASE_URL="$(heroku config:get DATABASE_URL)" -d sg-meetup-discord:latest
 docker container ls

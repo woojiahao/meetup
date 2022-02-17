fb6e018a6b72ea86cbed02a9a29858d06ec32fe0
woojiahao <woojiahao1234@gmail.com>
2022-02-17 21:58:53 +0800

    Migrate from khttp to Fuel
    khttp doesn't work for Java 9 and above

--- src/main/kotlin/com/github/woojiahao/api/EngineersSGAPI.kt
+++ src/main/kotlin/com/github/woojiahao/api/EngineersSGAPI.kt
@@ -1,9 +1,6 @@
 package com.github.woojiahao.api
 
-import com.google.gson.FieldNamingPolicy
-import com.google.gson.GsonBuilder
-import com.google.gson.JsonObject
-import khttp.get
+import com.github.kittinunf.fuel.httpGet
 import com.github.woojiahao.models.GeneralEvent
 import com.github.woojiahao.models.engineers_sg.Event
 import com.github.woojiahao.utility.date
@@ -11,16 +8,18 @@ import com.github.woojiahao.utility.extensions.read
 import com.github.woojiahao.utility.extractTimezoneDate
 import com.github.woojiahao.utility.time
 import com.github.woojiahao.utility.toSingaporeDateTime
+import com.google.gson.FieldNamingPolicy
+import com.google.gson.GsonBuilder
+import com.google.gson.JsonObject
 
 class EngineersSGAPI : IMeetupAPI {
   private val gson = GsonBuilder().setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES).create()
   private val endpoint = "https://engineers.sg/api/events"
 
   private fun queryEvents(): List<Event> {
-    with(get(endpoint)) {
-      return if (statusCode >= 400) emptyList()
-      else gson.read<JsonObject>(text)["events"].asJsonArray.map { gson.read<Event>(it.asJsonObject.toString()) }
-    }
+    val (_, response, _) = endpoint.httpGet().responseString()
+    return if (response.statusCode >= 400) emptyList()
+    else gson.read<JsonObject>(String(response.data))["events"].asJsonArray.map { gson.read<Event>(it.asJsonObject.toString()) }
   }
 
   /***
@@ -43,6 +42,7 @@ class EngineersSGAPI : IMeetupAPI {
 
   override fun getEvents(size: Int?): List<GeneralEvent> =
       with(queryEvents().map { it.generalise() }) {
-        size?.let { subList(0, it + 1) } ?: this
+        if (this.isEmpty()) emptyList<GeneralEvent>()
+        else size?.let { subList(0, it + 1) } ?: this
       }
 }
